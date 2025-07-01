609f207a9d5325ef55e358e976b824a2b8da6567
yujiawong <wyujia@umich.edu>
2025-07-01 15:18:57 +0800

    adding chat history deleteConfirm

--- src/components/qianwen/EditChat.vue
+++ src/components/qianwen/EditChat.vue
@@ -29,22 +29,66 @@
       </button> -->
       <ManageChatInEditPanel />
       <button
+        @click="handleShowDeleteConfirm"
         class="w-[150px] flex justify-start items-center gap-2 p-2 px-2 pr-4 hover:bg-red-200 text-red-500 border-t border-t-gray-200 rounded-b-sm"
       >
         <DeleteOutlined />
         <span>删除此对话</span>
       </button>
     </div>
+    <Teleport to="body">
+      <div
+        v-if="showDeleteConfirm"
+        id="deleteConfirm"
+        class="absolute top-0 left-0 w-screen h-screen z-40 bg-[rgb(0,0,0,0.5)] flex justify-center items-center"
+      >
+        <div
+          class="w-[400px] bg-white rounded-2xl flex flex-col justify-start items-start gap-2 pt-4 p-6"
+        >
+          <div class="flex items-center justify-start gap-2">
+            <ExclamationCircleOutlined class="!text-red-500" />
+            <span class="font-semibold">确认删除这 1 条对话记录吗？</span>
+          </div>
+          <span class="text-gray-500 ml-6"
+            >删除后对话记录无法恢复和找回，请谨慎操作</span
+          >
+          <div class="w-full flex items-end justify-end gap-2">
+            <button
+              class="rounded-2xl border border-gray-300 p-1 w-[80px] hover:text-[#615ced] hover:bg-[#615ced33]"
+              @click="handleUnshowDeleteConfirm"
+            >
+              取消
+            </button>
+            <button
+              class="rounded-2xl p-1 w-[80px] bg-red-500 text-white hover:bg-red-600"
+            >
+              确认删除
+            </button>
+          </div>
+        </div>
+      </div>
+    </Teleport>
   </div>
 </template>
 
 <script setup>
 import {
   EditOutlined,
+  ExclamationCircleOutlined,
   UnorderedListOutlined,
   PushpinOutlined,
   SendOutlined,
   DeleteOutlined,
 } from "@ant-design/icons-vue";
 import ManageChatInEditPanel from "./ManageChatInEditPanel.vue";
+import NewPromptAdding from "./NewPromptAdding.vue";
+import { ref } from "vue";
+
+let showDeleteConfirm = ref(false);
+function handleShowDeleteConfirm() {
+  showDeleteConfirm.value = true;
+}
+function handleUnshowDeleteConfirm() {
+  showDeleteConfirm.value = false;
+}
 </script>
